eef7d2c4b0809654f52554182d3bee02bc3668ec
Fabian Große <saphareas@googlemail.com>
2018-02-19 22:21:06 +0100

    Erste Edge Testversion
    "White Flash", Edge führt Content-Scipt zu schnell aus

--- pack.bat
+++ pack.bat
@@ -1,7 +1,9 @@
 mkdir temp
 xcopy /s _shared\* temp
 xcopy /s chrome\* temp
-7z a -tzip chrome.zip .\temp\*
+7z a -tzip ps-dark-chrome.zip .\temp\*
 xcopy /s /y firefox\* temp
-7z a -tzip firefox.zip .\temp\*
+7z a -tzip ps-dark-firefox.zip .\temp\*
+xcopy /s /y edge\* temp
+7z a -tzip ps-dark-edge.zip .\temp\*
 rd /s /q temp
