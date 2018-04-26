2c88c7f639cab41beae8fcd02afb227c86678ed3
Fabian Große <saphareas@googlemail.com>
2018-04-26 16:15:55 +0200

    Dunkles CSS für Playlisten und Profilseite

--- pack.bat
+++ pack.bat
@@ -1,7 +1,7 @@
 mkdir temp
 xcopy /s _shared\* temp
 xcopy /s chrome\* temp
-7z a -tzip chrome.zip .\temp\*
+7z a -tzip .releases\chrome.zip .\temp\*
 xcopy /s /y firefox\* temp
-7z a -tzip firefox.zip .\temp\*
+7z a -tzip .releases\firefox.zip .\temp\*
 rd /s /q temp
