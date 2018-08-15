b9241db71cf4eac7b4409d6478f10133e29f41f8
Fabian Große <saphareas@googlemail.com>
2018-08-16 01:13:01 +0200

    Fixes für die "neue" pietsmiet.de und Code aufgeräumt

--- chrome/content.js
+++ chrome/content.js
@@ -3,9 +3,8 @@ const POD_CSS = chrome.runtime.getURL("darken_podcast.css"); //Voll-qualifiziert
 var isFirstLoad = true;
 
 function _podcastHelper() {
-    var title = document.getElementsByTagName("title")[0].innerText;
+    var title = document.getElementsByClassName("mr-3 active")[0].innerText;
     if (title.includes("Podcast")) {
-        window.addEventListener("load", function() {_podcastHelper(element2);});
         var element2 = document.createElement("link");
         element2.setAttribute("id", "darkmode"); //id, um es später wieder zu finden
         element2.setAttribute("rel", "stylesheet");
@@ -13,6 +12,7 @@ function _podcastHelper() {
         element2.setAttribute("href", POD_CSS);
         var iFrame = document.getElementById("blockrandom");
         iFrame.contentWindow.document.getElementsByTagName("html")[0].appendChild(element2);
+        console.log("Podcast darkend");
     }
 }
 
@@ -23,39 +23,27 @@ function onToggleMode(request, sender) {
         element.setAttribute("rel", "stylesheet");
         element.setAttribute("type", "text/css");
         element.setAttribute("href", MAIN_CSS);
-        document.getElementsByTagName("head")[0].appendChild(element); //hänge an den Head an
+        document.getElementsByTagName("html")[0].appendChild(element); //hänge an das DOM an
         if (isFirstLoad) {
             window.addEventListener("load", function() {_podcastHelper();});
             isFirstLoad = false;
+            _podcastHelper(); /* wirft fast immer einen Error, aber ohne ist es buggy */
         } else {
             _podcastHelper();
         }
-
         chrome.storage.local.set({isDark: true});
-    } else { //also wenn auf normal gewechselt werden soll
+    }
+    else { //also wenn auf normal gewechselt werden soll
         document.getElementById("darkmode").remove(); //entferne vorher erstelltes link-Element aus DOM
-
-        var title = document.getElementsByTagName("title")[0].innerText;
+        var title = document.getElementsByClassName("mr-3 active")[0].innerText;
         if (title.includes("Podcast")) {
-            hasIFrame = document.getElementsByClassName("contentpane")[0].children[0];
-            if (hasIFrame) {
-                hasIFrame.contentWindow.document.getElementById("darkmode").remove();
-            }
+            var iFrame = document.getElementById("blockrandom");
+            iFrame.contentWindow.document.getElementById("darkmode").remove();
+            console.log("Podcast lightend");
         }
-
         chrome.storage.local.set({isDark: false});
     }
 }
 
 chrome.runtime.sendMessage("show the damn icon"); // Sende irgendwas zum Background, um dort die TabID zu haben
 chrome.runtime.onMessage.addListener(onToggleMode); //Sobald das Content-Script eine Message erhält, onToggleMode ausführen
-
-// Antworten-Button Fix
-window.addEventListener("load", function() { //läuft, wenn die Seite geladen wurde
-    setTimeout(function() { //1 sec Delay, weil Kommentare erst nachträglich geladen werden
-        var comments = document.getElementsByClassName("comment-body");
-        for (i=0; i < comments.length; i++) {
-            comments[i].parentNode.children[2].setAttribute("class","comments-buttons");
-        }
-    }, 1000);
-});
