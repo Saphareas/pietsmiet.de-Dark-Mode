8c0c4425c5d1e9fa652031a0b58852c9c63a0805
Fabian Große <saphareas@googlemail.com>
2018-12-20 17:05:07 +0100

    Move to GPLv3

--- changelog.js
+++ changelog.js
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) 2018 Fabian Große
+ * Released under the GNU GENERAL PUBLIC LICENSE 3
+ * https://github.com/Saphareas/pietsmiet.de-Dark-Mode/blob/master/LICENSE
+ */
+
 function handleOnInstalled(details) {
     let manifest = browser.runtime.getManifest();
     if (details.reason == "install") {
@@ -8,7 +14,8 @@ function handleOnInstalled(details) {
             type: "basic",
             title: `Version ${manifest.version} Changelog`,
             message: `${manifest.name} wurde aktualisiert. Das hat sich geändert:
-• Ein paar kleinere Style-Anpassungen, z.B. helle Input-Felder`,
+• Bug auf der Landing page gefixt
+• Ab jetzt lizensiert unter der GPLv3`,
             iconUrl: "icons/icon-48.png"
         };
         browser.notifications.create(notificationOptions);
